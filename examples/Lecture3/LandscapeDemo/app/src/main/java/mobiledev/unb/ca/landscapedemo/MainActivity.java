6d26add25008b777e71e1cc2e885d85a87e5b405
Howard Powell <hpowell@deloitte.ca>
2020-01-03 14:49:16 -0400

    Updates to the Landscape demo to show different labels and text when switching orientations

--- examples/Lecture3/LandscapeDemo/app/src/main/java/mobiledev/unb/ca/landscapedemo/MainActivity.java
+++ examples/Lecture3/LandscapeDemo/app/src/main/java/mobiledev/unb/ca/landscapedemo/MainActivity.java
@@ -1,30 +1,42 @@
 package mobiledev.unb.ca.landscapedemo;
 
 import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Resources resources;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
+        button = findViewById(R.id.button);
+        resources = button.getResources();
+
         button.setOnClickListener(new View.OnClickListener() {
+            @Override
             public void onClick(View src) {
                 Context context = getApplicationContext();
-                String text = getString(R.string.hello);
-                int duration = Toast.LENGTH_SHORT;
 
-                Toast toast = Toast.makeText(context, text, duration);
-                toast.show();
+                if (resources.getConfiguration().orientation == ORIENTATION_PORTRAIT) {
+                    Toast.makeText(context,
+                              "Running in Portrait Mode",
+                                   Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(context,
+                              "Running in Landscape Mode",
+                                   Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
