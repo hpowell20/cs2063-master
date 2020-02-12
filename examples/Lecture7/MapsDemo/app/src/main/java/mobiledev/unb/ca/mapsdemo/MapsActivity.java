9e6796dc96dfc9749dc5a38e678d2d4c2f3b4a69
Howard Powell <hpowell@deloitte.ca>
2020-02-12 15:20:43 -0400

    Addition of location details into the map view

--- examples/Lecture7/MapsDemo/app/src/main/java/mobiledev/unb/ca/mapsdemo/MapsActivity.java
+++ examples/Lecture7/MapsDemo/app/src/main/java/mobiledev/unb/ca/mapsdemo/MapsActivity.java
@@ -1,29 +1,73 @@
 package mobiledev.unb.ca.mapsdemo;
 
-import android.support.v4.app.FragmentActivity;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
+    private static final String TAG = "MapsActivity";
+    private static final int LOCATION_REQUEST = 101;
+
     private GoogleMap mMap;
+    private FusedLocationProviderClient fusedLocationClient;
+    private Location currentLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+
+        // Create an instance of the FusedLocationProviderClient
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        // Fetch the last known location
+        fetchLastLocation();
     }
 
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST);
+
+            // Location permissions not available
+            return;
+        }
+
+        Task task = fusedLocationClient.getLastLocation();
+        task.addOnSuccessListener(this, new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (location != null) {
+                    currentLocation = location;
+                }
+
+                Toast.makeText(getApplicationContext(), currentLocation.getLatitude() + "," +
+                        currentLocation.getLongitude(), Toast.LENGTH_LONG).show();
+
+                // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+                SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                        .findFragmentById(R.id.map);
+                mapFragment.getMapAsync(MapsActivity.this);
+
+            }
+        });
+    }
 
     /**
      * Manipulates the map once available.
@@ -38,9 +82,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Fredericton and move the camera
-        LatLng fredericton = new LatLng(45.957319, -66.647818);
-        mMap.addMarker(new MarkerOptions().position(fredericton).title("Fredericton!"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(fredericton, 5));
+        // Add a marker based on the current location and move the camera
+        LatLng latLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
+        MarkerOptions markerOptions = new MarkerOptions().position(latLng).title("You are here");
+        //mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+        //mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+        mMap.addMarker(markerOptions);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 5));
+    }
+
+    /**
+     * Handles the result of the permissions check
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,  String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case LOCATION_REQUEST: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Log.i(TAG, "onRequestPermissionsResult: Granted");
+                    fetchLastLocation();
+                } else {
+                    Toast.makeText(this, "onRequestPermissionsResult: Denied", Toast.LENGTH_SHORT);
+                    Log.i(TAG, "onRequestPermissionsResult: Denied");
+                }
+            }
+        }
     }
 }
