13554d1668e46da99410b04010c56d0ca70332d8
Howard Powell <hpowell@deloitte.ca>
2020-02-12 09:41:49 -0400

    Updated project to remove Google Client API references

--- examples/Lecture7/FusedLocationProviderDemo/app/src/main/java/mobiledev/unb/ca/fusedlocationproviderdemo/MainActivity.java
+++ examples/Lecture7/FusedLocationProviderDemo/app/src/main/java/mobiledev/unb/ca/fusedlocationproviderdemo/MainActivity.java
@@ -1,11 +1,14 @@
 package mobiledev.unb.ca.fusedlocationproviderdemo;
 
 import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import android.os.Looper;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,27 +17,21 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
-public class MainActivity extends AppCompatActivity implements ConnectionCallbacks,
-        OnConnectionFailedListener, View.OnClickListener {
+public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "TAG";
     private static final int LOCATION_REQUEST = 101;
 
     private Button mButton;
     private TextView mTextView;
-    private GoogleApiClient mGoogleApiClient;
-    //private LocationRequest locationRequest;
     private FusedLocationProviderClient fusedLocationClient;
-    //private LocationCallback locationCallback;
-    //private boolean requestingLocationUpdates = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,97 +40,99 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
 
         mTextView = findViewById(R.id.textview);
         mButton = findViewById(R.id.button);
-        //mButton.setEnabled(false);
+        mButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        getLastLocation();
+                    }
+                });
 
         // Create an instance of the FusedLocationProviderClient
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        /*initLocationClient();
-
-        locationCallback = new LocationCallback() {
-            @Override
-            public void onLocationResult(LocationResult locationResult) {
-                if (locationResult == null) {
-                    return;
-                }
-
-                for (Location location : locationResult.getLocations()) {
-                    String latitude = String.valueOf(location.getLatitude());
-                    String longitude = String.valueOf(location.getLongitude());
-                    String accuracy = String.valueOf(location.getAccuracy());
-
-                    String text = getString(R.string.location_details, latitude, longitude, accuracy);
-                    mTextView.setText(text);
-                }
-            };
-        };*/
-        initGoogleApiClient();
     }
 
-    private void initGoogleApiClient() {
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
-    }
-
-    /*private void initLocationClient() {
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
-        createLocationRequest();
-    }*/
-
-    /*private void createLocationRequest() {
-        locationRequest = LocationRequest.create();
-        locationRequest.setInterval(10000);
-        locationRequest.setFastestInterval(5000);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-    }*/
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        //if (requestingLocationUpdates) {
-            //startLocationUpdates();
-        //}
-
-        if (null != mGoogleApiClient) {
-            mGoogleApiClient.connect();
+    private void getLastLocation() {
+        if (checkPermissions()) {
+            if (isLocationEnabled()) {
+                fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location lastLocation) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (lastLocation != null) {
+                                setTextViewDisplay(lastLocation);
+                            } else {
+                                mTextView.setText("Unable to fetch the location");
+                                requestNewLocationData();
+                            }
+                        }
+                    });
+            } else {
+                Toast.makeText(this, "Please turn location services on", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(intent);
+            }
+        } else {
+            requestPermissions();
         }
     }
 
-    /*private void startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(locationRequest,
-                locationCallback,
-                Looper.getMainLooper());
-    }*/
+    private void setTextViewDisplay(Location location) {
+        String latitude = String.valueOf(location.getLatitude());
+        String longitude = String.valueOf(location.getLongitude());
+        String accuracy = String.valueOf(location.getAccuracy());
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-
-        if (null != mGoogleApiClient) {
-            mGoogleApiClient.disconnect();
-        }
-        //stopLocationUpdates();
+        String text = getString(R.string.location_details, latitude, longitude, accuracy);
+        mTextView.setText(text);
     }
 
-    /*private void stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback);
-    }*/
+    /**
+     * Method to determine if the user has granted the appropriate access levels
+     * @return Flag indicating grant access level
+     */
+    private boolean checkPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Grants the appropriate permissions
+     */
+    private void requestPermissions() {
+        ActivityCompat.requestPermissions(this, new String[] {
+                Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
+        }, LOCATION_REQUEST);
+    }
+
+    /**
+     * Checks to see if the user has turned on location from Settings
+     * @return
+     */
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+                LocationManager.NETWORK_PROVIDER
+        );
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode,
                                            String permissions[],
                                            int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
         switch (requestCode) {
             case LOCATION_REQUEST: {
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.i(TAG, "onRequestPermissionsResult: Granted");
-                    getCurrentLocation();
-
+                    getLastLocation();
                 } else {
                     Toast.makeText(this, "onRequestPermissionsResult: Denied", Toast.LENGTH_SHORT);
                     Log.i(TAG, "onRequestPermissionsResult: Denied");
@@ -142,71 +141,25 @@ public class MainActivity extends AppCompatActivity implements ConnectionCallbac
         }
     }
 
-    @Override
-    public void onConnected(@Nullable Bundle bundle) {
-        // Connected to Google Play services
-        mButton.setEnabled(true);
-        mButton.setOnClickListener(this);
+    private void requestNewLocationData(){
+
+        LocationRequest mLocationRequest = new LocationRequest();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(0);
+        mLocationRequest.setFastestInterval(0);
+        mLocationRequest.setNumUpdates(1);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        fusedLocationClient.requestLocationUpdates(
+                mLocationRequest, mLocationCallback,
+                Looper.myLooper()
+        );
     }
 
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection has been interrupted.
-        // Disable any UI components that depend on Google APIs
-        // until onConnected() is called.
-        mButton.setEnabled(false);
-        Toast.makeText(this, "Connection was suspended", Toast.LENGTH_SHORT);
-    }
-
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult result) {
-        // This callback is important for handling errors that
-        // may occur while attempting to connect with Google.
-        //
-        // More about this in the 'Handle Connection Failures' section.
-        mButton.setEnabled(false);
-        Toast.makeText(this, "Connection failed", Toast.LENGTH_SHORT);
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = "
-                + result.getErrorCode());
-    }
-
-    private void getCurrentLocation() {
-        // Check to see if the location permission is granted
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[] {
-                    Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION
-            }, LOCATION_REQUEST);
-
-            Toast.makeText(this, "Location permission check failed", Toast.LENGTH_SHORT);
-            Log.i(TAG, "Get location without permissions...");
-            return;
+    private LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            setTextViewDisplay(locationResult.getLastLocation());
         }
-
-        fusedLocationClient.getLastLocation()
-                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
-                    @Override
-                    public void onSuccess(Location lastLocation) {
-                        // Got last known location. In some rare situations this can be null.
-                        if (lastLocation != null) {
-                            String latitude = String.valueOf(lastLocation.getLatitude());
-                            String longitude = String.valueOf(lastLocation.getLongitude());
-                            String accuracy = String.valueOf(lastLocation.getAccuracy());
-
-                            String text = getString(R.string.location_details, latitude, longitude, accuracy);
-                            mTextView.setText(text);
-                        } else {
-                            mTextView.setText("Unable to fetch the location");
-                        }
-                    }
-                });
-    }
-
-    @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.button:
-                getCurrentLocation();
-                break;
-        }
-    }
+    };
 }
