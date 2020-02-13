85894e17dd6cb353d99a3728ecf12a9c69988742
Howard Powell <hspowell@gmail.com>
2020-02-13 11:04:07 -0400

    Reverted the androidx imports that were not being used

--- examples/Lecture7/MapsDemo/app/src/main/java/mobiledev/unb/ca/mapsdemo/MapsActivity.java
+++ examples/Lecture7/MapsDemo/app/src/main/java/mobiledev/unb/ca/mapsdemo/MapsActivity.java
@@ -4,11 +4,10 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.widget.Toast;
-
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
