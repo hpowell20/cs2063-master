a264dd771693b65bc77236fe37f036f05ff0bd34
Howard Powell <hpowell@deloitte.ca>
2020-02-14 11:34:05 -0400

    Addition of Lab 6 solution folder

---
+++ lab-solutions/Lab6/AsyncTaskSkeleton/app/src/main/java/mobiledev/unb/ca/asynctasklab/GeoDataDetailFragment.java
@@ -0,0 +1,74 @@
+package mobiledev.unb.ca.asynctasklab;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * A fragment representing a single Course detail screen.
+ * This fragment is either contained in a {@link GeoDataListActivity}
+ * in two-pane mode (on tablets) or a {@link GeoDataDetailActivity}
+ * on handsets.
+ */
+public class GeoDataDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String TITLE = "title";
+    public static final String LNG = "item_longitutde";
+    public static final String LAT = "item_latitude";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private String title;
+    private String lng;
+    private String lat;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public GeoDataDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Bundle arguments = getArguments();
+        if (arguments != null && arguments.containsKey(LNG)) {
+            title = arguments.getString(TITLE);
+            lng = arguments.getString(LNG);
+            lat = arguments.getString(LAT);
+
+            Activity activity = this.getActivity();
+            if (activity != null) {
+                CollapsingToolbarLayout appBarLayout = activity.findViewById(R.id.toolbar_layout);
+                if (appBarLayout != null) {
+                    appBarLayout.setTitle("Earthquake Details");
+                }
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.geodata_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (lat != null && lng != null) {
+            String text = getString(R.string.fragment_dummy_content, title, lng, lat);
+            ((TextView) rootView.findViewById(R.id.course_detail)).setText(text);
+        }
+
+        return rootView;
+    }
+}
---
+++ lab-solutions/Lab6/AsyncTaskSkeleton/app/src/main/java/mobiledev/unb/ca/asynctasklab/util/JsonUtils.java
@@ -0,0 +1,122 @@
+package mobiledev.unb.ca.asynctasklab.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.net.HttpURLConnection;
+import java.util.ArrayList;
+
+import javax.json.Json;
+import javax.json.stream.JsonParser;
+
+import mobiledev.unb.ca.asynctasklab.model.GeoData;
+
+public class JsonUtils {
+    private static final String REQUEST_URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson";
+    private static final String JSON_KEY_TITLE = "title";
+    private static final String JSON_KEY_COORDINATES = "coordinates";
+
+    private ArrayList<GeoData> geoDataArray;
+
+    // Initializer to read our data source (JSON file) into an array of course objects
+    public JsonUtils() {
+        processJSON();
+    }
+
+    private void processJSON() {
+        String jsonString = loadJSONFromURL();
+        JsonParser parser = Json.createParser(new StringReader(jsonString));
+        boolean titleTrigger = false;
+        boolean coordTrigger = false;
+        int count = 0;
+        int coordCount = 0;
+
+        try {
+            while(parser.hasNext()) {
+                JsonParser.Event event = parser.next();
+                switch (event) {
+                    case KEY_NAME:
+                        if(parser.getString().equals(JSON_KEY_TITLE)) {
+                            titleTrigger = true;
+                        }
+                        else if (parser.getString().equals(JSON_KEY_COORDINATES)) {
+                            coordTrigger = true;
+                        }
+                        break;
+                    case VALUE_STRING:
+                        if(titleTrigger && parser.getString().startsWith("M")) {
+                            GeoData geoData = new GeoData();
+                            geoData.setTitle(parser.getString());
+                            geoDataArray.add(geoData);
+                            titleTrigger = false;
+                        }
+                        break;
+                    case VALUE_NUMBER:
+                        if(coordTrigger && (coordCount == 0) ) {
+                            GeoData geoData = geoDataArray.get(count);
+                            geoData.setLongitude(parser.getString());
+                            coordCount++;
+                        }
+                        else if(!coordTrigger && (coordCount == 1)) {
+                            GeoData geoData = geoDataArray.get(count);
+                            geoData.setLatitude(parser.getString());
+                            coordCount = 0;
+                            count++;
+                        }
+                        coordTrigger = false;
+                        break;
+
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String loadJSONFromURL() {
+        try {
+            HttpURLConnection connection = null;
+
+            // TODO
+            //  Establish an HttpURLConnecion to requestURL
+            //  Hint: See https://github.com/hpowell20/cs2063-winter-2020-examples/tree/master/Lecture4/NetworkingURL
+            //  for an example of how to do this
+            //  Also see documentation here:
+            //  http://developer.android.com/training/basics/network-ops/connecting.html
+
+            return convertStreamToString(connection.getInputStream());
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Getter method for courses ArrayList
+    public ArrayList<GeoData> getGeoData() {
+        return geoDataArray;
+    }
+}
