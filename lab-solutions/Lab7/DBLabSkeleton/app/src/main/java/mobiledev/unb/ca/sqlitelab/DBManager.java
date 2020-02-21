faf7e1c6d018a8696d49dd64e5102fbf84848903
Howard Powell <hspowell@gmail.com>
2020-02-21 11:49:55 -0400

    Addition of SQLite code for Add and Query

--- lab-solutions/Lab7/DBLabSkeleton/app/src/main/java/mobiledev/unb/ca/sqlitelab/DBManager.java
+++ lab-solutions/Lab7/DBLabSkeleton/app/src/main/java/mobiledev/unb/ca/sqlitelab/DBManager.java
@@ -1,7 +1,10 @@
 package mobiledev.unb.ca.sqlitelab;
 
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 
 
 public class DBManager {
@@ -18,6 +21,29 @@ public class DBManager {
         dbHelper.close();
     }
 
-    // TODO
-    //  Add database query operations in this class
+    public Cursor listAllRecordsByItem(String item) {
+        Cursor cursor = openReadOnlyDatabase().query(DatabaseHelper.TABLE_NAME,
+                DatabaseHelper.COLUMNS, "item=?", new String[]{item},
+                null, null, DatabaseHelper.NUM + " ASC");
+        if (cursor != null) {
+            cursor.moveToFirst();
+        }
+
+        return cursor;
+    }
+
+    public void insertRecord(String item, int num) {
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(DatabaseHelper.ITEM, item);
+        contentValue.put(DatabaseHelper.NUM, num);
+        openWriteDatabase().insert(DatabaseHelper.TABLE_NAME, null, contentValue);
+    }
+
+    private SQLiteDatabase openReadOnlyDatabase() {
+        return dbHelper.getReadableDatabase();
+    }
+
+    private SQLiteDatabase openWriteDatabase() {
+        return dbHelper.getWritableDatabase();
+    }
 }
--- lab-solutions/Lab7/DBLabSkeleton/app/src/main/java/mobiledev/unb/ca/sqlitelab/MainActivity.java
+++ lab-solutions/Lab7/DBLabSkeleton/app/src/main/java/mobiledev/unb/ca/sqlitelab/MainActivity.java
@@ -1,21 +1,28 @@
 package mobiledev.unb.ca.sqlitelab;
 
+import android.content.Context;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
+import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+
+    // Cursor query attributes
+    private final String[] FROM = {DatabaseHelper.ITEM, DatabaseHelper.NUM};
+    private final int[] TO = {R.id.item_textview, R.id.num_textview};
 
     private DBManager mDBManager;
 
@@ -24,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText mItemEditText;
     private EditText mNumberEditText;
     private TextView mResultsTextView;
-    private ListView mListview;
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +47,7 @@ public class MainActivity extends AppCompatActivity {
         mItemEditText = findViewById(R.id.item_edit_text);
         mNumberEditText = findViewById(R.id.number_edit_text);
         mResultsTextView = findViewById(R.id.results_text_view);
-        mListview = findViewById(R.id.listview);
+        mListView = findViewById(R.id.listview);
 
         mAddButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,13 +56,21 @@ public class MainActivity extends AppCompatActivity {
                 //  EditTexts. If so, create and execute an AddTask, passing its
                 //  doInBackground method the text from these EditTexts. If not,
                 //  display a toast indicating that the data entered was incomplete.
+               Context context = getApplicationContext();
                String itemText =  mItemEditText.getText().toString();
-               int itemNum = 0;
-               try {
-                   Integer.parseInt(mNumberEditText.getText().toString());
-               } catch (NumberFormatException nfe) {
-                   Log.w(TAG, "Error parsing the number value. " + nfe.getMessage());
+               if (TextUtils.isEmpty(itemText)) {
+                   Toast.makeText(context, getString(R.string.err_no_item_value_entered), Toast.LENGTH_SHORT).show();
+                   return;
                }
+
+               String numberText = mNumberEditText.getText().toString();
+               if (TextUtils.isEmpty(numberText)) {
+                   Toast.makeText(context, getString(R.string.err_no_number_value_entered), Toast.LENGTH_SHORT).show();
+                   return;
+               }
+
+               AddTask addTask = new AddTask();
+               addTask.execute(itemText, numberText);
             }
         });
 
@@ -66,6 +81,10 @@ public class MainActivity extends AppCompatActivity {
                     // TODO v is the search EditText. (EditText is a subclass of TextView.)
                     //  Get the text from this view. Create and execute a QueryTask passing
                     //  its doInBackground method this text.
+                    QueryTask queryTask = new QueryTask();
+                    queryTask.execute(v.getText().toString());
+
+                    return true;
                 }
 
                 return false;
@@ -82,6 +101,13 @@ public class MainActivity extends AppCompatActivity {
         protected Void doInBackground(String... params) {
             // TODO Get the item and number that were passed to this method
             //  as params. Add a corresponding row to the the database.
+            try {
+                String item = params[0];
+                int num = Integer.parseInt(params[1]);
+                mDBManager.insertRecord(item, num);
+            } catch (NullPointerException npe) {
+                Toast.makeText(getApplicationContext(), getString(R.string.err_not_numeric_value), Toast.LENGTH_SHORT).show();
+            }
 
             return null;
         }
@@ -94,6 +120,7 @@ public class MainActivity extends AppCompatActivity {
             //  changes elsewhere in the app. Exactly how you make the
             //  UI behave is up to you, but you should make reasonable
             //  choices.
+            Toast.makeText(getApplicationContext(), getString(R.string.msg_record_added), Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -104,9 +131,7 @@ public class MainActivity extends AppCompatActivity {
             //  retrieve all rows that have an item that matches this query,
             //  and return this Cursor object. Make sure that the results
             //  are sorted appropriately.
-
-            // TODO Remove this return statement when you're done this part
-            return null;
+            return mDBManager.listAllRecordsByItem(params[0]);
         }
 
         protected void onPostExecute(Cursor result) {
@@ -116,6 +141,25 @@ public class MainActivity extends AppCompatActivity {
             //  results TextView to indicate that there are no results.
             //  Again, you might need to write a bit of extra code here,
             //  or elsewhere, to get the UI to behave nicely.
+            int cursorCount = result.getCount();
+            if (cursorCount <= 0) {
+                mResultsTextView.setText(getString(R.string.msg_no_results_found));
+            } else {
+                String text = cursorCount == 1 ?
+                        getString(R.string.msg_single_result_found, cursorCount) :
+                        getString(R.string.msg_multiple_results_found, cursorCount);
+                mResultsTextView.setText(text);
+            }
+
+            SimpleCursorAdapter adapter = new SimpleCursorAdapter(getApplicationContext(),
+                    R.layout.list_layout,
+                    result,
+                    FROM,
+                    TO,
+                    0);
+
+            adapter.notifyDataSetChanged();
+            mListView.setAdapter(adapter);
         }
     }
 }
