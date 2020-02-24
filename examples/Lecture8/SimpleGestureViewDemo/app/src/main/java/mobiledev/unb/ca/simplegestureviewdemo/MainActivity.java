327f02a7f481e93d9c670a155090e19f450c9755
Howard Powell <hpowell@deloitte.ca>
2020-02-24 13:52:52 -0400

    Updates to the SimpleGestureViewDemo project

--- examples/Lecture8/SimpleGestureViewDemo/app/src/main/java/mobiledev/unb/ca/simplegestureviewdemo/MainActivity.java
+++ examples/Lecture8/SimpleGestureViewDemo/app/src/main/java/mobiledev/unb/ca/simplegestureviewdemo/MainActivity.java
@@ -1,17 +1,11 @@
 package mobiledev.unb.ca.simplegestureviewdemo;
 
-import android.support.v4.view.GestureDetectorCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GestureDetectorCompat;
 
 public class MainActivity extends AppCompatActivity {
-
 
     private GestureDetectorCompat mDetector;
 
@@ -20,36 +14,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mDetector = new GestureDetectorCompat(this, new MyGestureListener());
-        TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return mDetector.onTouchEvent(event);
-            }
-        });
-    }
 
-    class MyGestureListener extends GestureDetector.SimpleOnGestureListener {
-        private static final String DEBUG_TAG = "Gestures";
-
-        @Override
-        public boolean onDown(MotionEvent event) {
-            Log.d(DEBUG_TAG,"onDown");
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent event1, MotionEvent event2,
-                               float velocityX, float velocityY) {
-            Log.d(DEBUG_TAG, "onFling: x velocity=" + Float.toString(velocityX)
-                    + " y velocity=" + Float.toString(velocityY));
-            return true;
-        }
-
-        public boolean onSingleTapConfirmed(MotionEvent event) {
-            Log.d(DEBUG_TAG, "onSingleTapConfirmed");
-            return true;
-        }
+        CustomTextView textView = findViewById(R.id.textView);
+        textView.setDetectorObject(mDetector);
     }
 }
 
