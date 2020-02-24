1a9b2649aa4e5ac4b534dadba169e92603f71b69
Howard Powell <hpowell@deloitte.ca>
2020-02-24 13:03:24 -0400

    Updates to the MotionEventDemo example project

--- examples/Lecture8/MotionEventDemo/app/src/main/java/mobiledev/unb/ca/motioneventdemo/MainActivity.java
+++ examples/Lecture8/MotionEventDemo/app/src/main/java/mobiledev/unb/ca/motioneventdemo/MainActivity.java
@@ -1,9 +1,10 @@
 package mobiledev.unb.ca.motioneventdemo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.MotionEvent;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
