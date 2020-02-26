443b2865763d4fe54d80da1f91793d6c8956239b
Howard Powell <hpowell@deloitte.ca>
2020-02-26 11:51:42 -0400

    Addition of SharedPreferences code

--- lab-solutions/LabExamSkeleton/app/src/main/java/mobiledev/unb/ca/labexam/MainActivity.java
+++ lab-solutions/LabExamSkeleton/app/src/main/java/mobiledev/unb/ca/labexam/MainActivity.java
@@ -1,6 +1,8 @@
 package mobiledev.unb.ca.labexam;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -25,6 +27,7 @@ public class MainActivity extends AppCompatActivity {
     private static final String PREFS_FILE_NAME = "LabExamPrefs";
 
     private RecyclerView mRecyclerView;
+    private SharedPreferences prefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: SharedPreferences
         //  Setup the instance of shared preferences you will be using
+        initSharedPreferences();
 
         // TODO
         //  Create an instance of LoadDataTask and execute it
@@ -77,28 +81,26 @@ public class MainActivity extends AppCompatActivity {
             // TODO
             //  Get the GamesInfo at index position in mDataSet
             //  (Hint: you might need to declare this variable as final.)
-            final GamesInfo city = mDataset.get(position);
+            final GamesInfo gamesInfo = mDataset.get(position);
 
             // TODO
             //  Set the TextView in the ViewHolder (holder) to be the name for the city
-            holder.mTextView.setText(city.getTitle());
-
-            // Nothing to do here; just leave this here and complete the other todos in place
-            holder.mCheckBox.setOnCheckedChangeListener(null);
+            holder.mTextView.setText(gamesInfo.getTitle());
 
             // TODO
             //  Set the onClickListener for the TextView in the ViewHolder (holder) such
             //  that when it is clicked, it creates an explicit intent to launch DetailActivity
             //  Hint: You will need to put extra pieces of information in this intent.
+            final String number = gamesInfo.getNumber();
             holder.mTextView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
                     Intent intent = new Intent(MainActivity.this, DetailActivity.class);
-                    intent.putExtra(Constants.INTENT_KEY_NUMBER, city.getNumber());
-                    intent.putExtra(Constants.INTENT_KEY_YEAR, city.getYear());
-                    intent.putExtra(Constants.INTENT_KEY_DATES, city.getDates());
-                    intent.putExtra(Constants.INTENT_KEY_HOST_CITY, city.getHostCity());
-                    intent.putExtra(Constants.INTENT_KEY_WIKIPEDIA_LINK, city.getWikipediaLink());
+                    intent.putExtra(Constants.INTENT_KEY_NUMBER, number);
+                    intent.putExtra(Constants.INTENT_KEY_YEAR, gamesInfo.getYear());
+                    intent.putExtra(Constants.INTENT_KEY_DATES, gamesInfo.getDates());
+                    intent.putExtra(Constants.INTENT_KEY_HOST_CITY, gamesInfo.getHostCity());
+                    intent.putExtra(Constants.INTENT_KEY_WIKIPEDIA_LINK, gamesInfo.getWikipediaLink());
 
                     if (intent.resolveActivity(getPackageManager()) != null) {
                         startActivity(intent);
@@ -113,6 +115,7 @@ public class MainActivity extends AppCompatActivity {
             //  preferences for this id, then the checkbox should not be checked
             //  (i.e., assume a default value of false for ids that are not in
             //  the shared preferences).
+            holder.mCheckBox.setChecked(prefs.getBoolean(number, false));
 
             // Hints:
             // https://developer.android.com/reference/android/content/SharedPreferences.html#getBoolean(java.lang.String,%20boolean)
@@ -121,7 +124,6 @@ public class MainActivity extends AppCompatActivity {
 
             holder.mCheckBox.setOnCheckedChangeListener(
                     new CompoundButton.OnCheckedChangeListener() {
-
                         // This method is called when a CheckBox is clicked, and its status
                         // changes from checked to not checked, or from not checked to checked.
                         // isChecked will be true if the CheckBox is now checked, and false if
@@ -130,16 +132,18 @@ public class MainActivity extends AppCompatActivity {
                             // TODO: SharedPreferences
                             //  Get a SharedPreferences.Editor for SharedPreferences
                             //  Hint: https://developer.android.com/reference/android/content/SharedPreferences.html#edit()
+                            SharedPreferences.Editor editor = prefs.edit();
 
                             // TODO: Shared Preferences
                             //  Set the value stored in SharedPreferences for the id for this GamesInfo to be
                             //  the value of isChecked
                             //  Hint: https://developer.android.com/reference/android/content/SharedPreferences.Editor.html#putBoolean(java.lang.String,%20boolean)
+                            editor.putBoolean(number, isChecked);
 
                             // TODO: SharedPreferences
                             //  Apply the changes from this editor
                             //  Hint: https://developer.android.com/reference/android/content/SharedPreferences.Editor.html#apply()
-
+                            editor.commit();
                         }
                     }
             );
@@ -169,5 +173,9 @@ public class MainActivity extends AppCompatActivity {
             MyAdapter myAdapter = new MyAdapter(result);
             mRecyclerView.setAdapter(myAdapter);
         }
+    }
+
+    private void initSharedPreferences() {
+        prefs = getSharedPreferences(PREFS_FILE_NAME, Context.MODE_PRIVATE);
     }
 }
