d5e713baae4d1caa24e347784fcc4a1d5c7d8ac5
Howard Powell <hpowell@deloitte.ca>
2020-03-09 14:09:32 -0300

    Updates to the CanvasDemo example project

--- examples/Lecture8/CanvasDemo/app/src/main/java/mobiledev/unb/ca/canvasdemo/MainActivity.java
+++ examples/Lecture8/CanvasDemo/app/src/main/java/mobiledev/unb/ca/canvasdemo/MainActivity.java
@@ -1,26 +1,13 @@
 package mobiledev.unb.ca.canvasdemo;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
-import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
 import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Random;
-
 public class MainActivity extends AppCompatActivity {
-    private static final String TAG = "TAG";
-    private static final int BITMAPSIZE = 64;
-
-    // Display dimensions
-    private int mDisplayWidth;
-    private int mDisplayHeight;
     private RelativeLayout mFrame;
 
     @Override
@@ -32,76 +19,16 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
+        // Set the bubble image from the drawable resource
         Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.b64);
-        final BubbleView bv = new BubbleView(getApplicationContext(), bitmap);
-        mFrame.addView(bv);
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while (bv.move()) {
-                    bv.postInvalidate();
-                    try {
-                        Thread.sleep(20);
-                    } catch (InterruptedException e) {
-                        Log.i(TAG, "InterruptedException");
-                    }
-                }
-            }
-        }).start();
-    }
-
-    private class BubbleView extends View {
-
-        final private Bitmap mBitmap;
-
-        private float mCurrX;
-        private float mCurrY;
-
-        private int mStepX;
-        private int mStepY;
-        private static final int MAXSTEP = 10;
-
-        final private Paint mPainter = new Paint();
-
-        public BubbleView(Context context, Bitmap bitmap) {
-            super(context);
-
-            this.mBitmap = Bitmap.createScaledBitmap(bitmap,
-                    BITMAPSIZE, BITMAPSIZE, false);
-
-            mDisplayWidth = mFrame.getWidth();
-            mDisplayHeight = mFrame.getHeight();
-
-            // Start in roughly the centre
-            mCurrX = mDisplayWidth / 2;
-            mCurrY = mDisplayHeight / 2;
-
-            Random r = new Random();
-            // Pick a random x and y step
-            mStepX = r.nextInt(MAXSTEP) + 1;
-            mStepY = r.nextInt(MAXSTEP) + 1;
-            // Pick a random x and y direction to step
-            mStepX = r.nextBoolean() ? mStepX : -mStepX;
-            mStepY = r.nextBoolean() ? mStepY : -mStepY;
-
-            mPainter.setAntiAlias(true);
-
-        }
-
-        protected void onDraw(Canvas canvas) {
-            canvas.drawBitmap(mBitmap, mCurrX, mCurrY, mPainter);
-        }
-
-        private boolean move() {
-            mCurrX = mCurrX + mStepX;
-            mCurrY = mCurrY + mStepY;
-
-            // Return true if the BubbleView is on the screen
-            return mCurrX <= mDisplayWidth &&
-                    mCurrX + BITMAPSIZE >= 0 &&
-                    mCurrY <= mDisplayHeight &&
-                    mCurrY + BITMAPSIZE >= 0;
+        // Start the animation with 5 balls
+        for (int i = 0; i < 5; i++) {
+            final BubbleView bubbleView = new BubbleView(getApplicationContext(),
+                    bitmap,
+                    mFrame.getWidth(),
+                    mFrame.getHeight());
+            mFrame.addView(bubbleView);
         }
     }
 }
