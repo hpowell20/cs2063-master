388e9db4a1fc663064a2f6749d15b479703fd564
Howard Powell <hspowell@gmail.com>
2022-01-16 07:40:09 -0400

    Updated Lab 2 base files

--- Winter 2022/Lab2/Lab2ActivityLifecycle/app/src/main/java/mobiledev/unb/ca/lab2activitylifecycle/ActivityOne.java
+++ Winter 2022/Lab2/Lab2ActivityLifecycle/app/src/main/java/mobiledev/unb/ca/lab2activitylifecycle/ActivityOne.java
@@ -1,6 +1,5 @@
 package mobiledev.unb.ca.lab2activitylifecycle;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,10 +7,11 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class ActivityOne extends Activity {
-
+public class ActivityOne extends AppCompatActivity {
     // Strings will serve as keys when saving state between activities
     private static final String CREATE_VALUE = "create";
     private static final String START_VALUE = "start";
@@ -28,19 +28,19 @@ public class ActivityOne extends Activity {
     // called by the system. Below are the variables you will use
     // to increment during each lifecycle method call. We will be
     // tracking only these four lifecycle methods during this lab.
-    private int mOnCreateCount = 0;
-    private int mOnStartCount = 0;
-    private int mOnResumeCount = 0;
-    private int mOnRestartCount = 0;
-    private Button mActivityTwoBtn;
+    private int onCreateCount = 0;
+    private int onStartCount = 0;
+    private int onResumeCount = 0;
+    private int onRestartCount = 0;
+    private Button activityTwoButton;
 
-    // TODO 2:
-    // Declare four private TextView Objects above. The will contain counts of the
-    // four lifecycle method calls indicated in the activity layout XML. To contain
-    // references to Android SDK Object types, they must be programmatically
-    // declared as instances of the object type, much like when
-    // declaring an int, double, or String. All rules of Java continue
-    // to apply.
+    // TODO 2
+    //  Declare four private TextView Objects. They will contain counts of the
+    //  four lifecycle method calls indicated in the activity layout XML.
+    //  NOTE:
+    //  To contain references to Android SDK Object types they must be programmatically
+    //  declared as instances of the object type, much like when
+    //  declaring an int, double, or String.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,46 +50,46 @@ public class ActivityOne extends Activity {
 
         // HINT for 4:
         // This is how Android View layout resource references are obtained
-        mActivityTwoBtn = (Button) findViewById(R.id.btnLaunchActivityTwo);
-        mActivityTwoBtn.setOnClickListener(v -> {
-            // HINT for 3:
-            // Intents are a way to announce to the Android operating system that
-            // your application intends to perform some request. These requests
-            // can be directly calling some specified Activity, as we will be doing
-            // here, or it can announce the intent of having some particular
-            // activity type respond to its request time; for instance indicating
-            // it will need access to an email application activity. We will
-            // investigate intents further in a future lab!
+        activityTwoButton = findViewById(R.id.btnLaunchActivityTwo);
+        activityTwoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // HINT for 3:
+                // Intents are a way to announce to the Android operating system that
+                // your application intends to perform some request. These requests
+                // can be directly calling some specified Activity, as we will be doing
+                // here, or it can announce the intent of having some particular
+                // activity type respond to its request time; for instance indicating
+                // it will need access to an email application activity. We will
+                // investigate intents further in a future lab!
 
-            // TODO 3:
-            // Launch the ActivityTwo class using the intent below. For more information,
-            // consult the Android API documentation for starting activities:
-            // http://developer.android.com/reference/android/app/Activity.html#startActivity(android.content.Intent)
-            Intent intent = new Intent(ActivityOne.this, ActivityTwo.class);
-
+                // TODO 3:
+                //  Launch the ActivityTwo class using the intent below. For more information,
+                //  consult the Android API documentation for starting activities:
+                //  https://developer.android.com/reference/android/app/Activity#startActivity(android.content.Intent)
+                Intent intent = new Intent(ActivityOne.this, ActivityTwo.class);
+            }
         });
 
-        // TODO 4:
-        // Use the above Button resource reference example to capture TextView
-        // references for mOnCreateText, mOnStartText, mOnResumeText, and
-        // mOnRestartText
+        // TODO 4
+        //  Use the above Button resource reference example to capture TextView
+        //  references for the four private TextView objects
 
 
         // HINT for 6:
         // This checks whether or not a savedInstanceState currently exists
         // If it does, counter values will be loaded from its previous state
         if (savedInstanceState != null) {
+            onCreateCount = savedInstanceState.getInt(CREATE_VALUE);
 
-            mOnCreateCount = savedInstanceState.getInt(CREATE_VALUE);
-
-            // TODO 6:
-            // If a savedInstanceState Bundle exists then there have already
-            // been system calls made to activity lifecycle methods. We can
-            // use this Bundle to set current values.
+            // TODO 6
+            //  If a savedInstanceState Bundle exists then there have already
+            //  been system calls made to activity lifecycle methods. We can
+            //  use this Bundle to set current values.
         }
 
-        // TODO 8:
-        // Increment mOnCreateCount
+        // TODO 8
+        //  Increment onCreateCount
 
         updateCountsDisplay();
     }
@@ -99,8 +99,8 @@ public class ActivityOne extends Activity {
         Log.i(TAG, "onStart() called");
         super.onStart();
 
-        // TODO 8:
-        // Increment mOnStartCount
+        // TODO 8
+        //  Increment onStartCount
 
         updateCountsDisplay();
     }
@@ -110,8 +110,8 @@ public class ActivityOne extends Activity {
         Log.i(TAG, "onResume() called");
         super.onResume();
 
-        // TODO 8:
-        // Increment mOnResumeCount
+        // TODO 8
+        //  Increment onResumeCount
 
         updateCountsDisplay();
     }
@@ -121,33 +121,32 @@ public class ActivityOne extends Activity {
         Log.i(TAG, "onRestart() called");
         super.onRestart();
 
-        // TODO 8:
-        // Increment mOnRestartCount
+        // TODO 8
+        //  Increment onRestartCount
 
         updateCountsDisplay();
     }
 
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(CREATE_VALUE, onCreateCount);
 
-        savedInstanceState.putInt(CREATE_VALUE, mOnCreateCount);
-
-        // TODO 5:
-        // Following the above example, save the current counters to a
-        // savedInstanceState Bundle so they can be refreshed when
-        // returning to this Activity. These behave as key:value pairs,
-        // which you may be familiar with if you've taken Info 1103!
-
+        // TODO 5
+        //  Following the above example, save the current counters to a
+        //  savedInstanceState Bundle so they can be refreshed when
+        //  returning to this Activity.
 
         // Must always call the superclass so it can save the view hierarchy state
         super.onSaveInstanceState(savedInstanceState);
     }
 
     public void updateCountsDisplay() {
-        // TODO 7:
-        // Update the TextView resources as defined in step 2 to update the associated counter
-        // For example:
-        //      YourOnCreateTextVariableName.setText("onCreate() calls: " + mOnCreateCount);
+        // TODO 7
+        //  Update all of the TextView resources to the correct counter
+        //  HINT:
+        //  Follow the provided example where yourOnCreateTextVariableName
+        //  refers to any of the private TextView objects
+        yourOnCreateTextVariableName.setText("onCreate() calls: " + onCreateCount);
     }
 
     @Override
