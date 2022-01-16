47f4a0b999a9f9d03e17a2851011aef35eaa0281
Howard Powell <hspowell@gmail.com>
2022-01-16 13:24:46 -0400

    Updated code

--- Winter 2022/examples/Lecture 2/java/LandscapeDemo/app/src/main/java/mobiledev/unb/ca/landscapedemo/MainActivity.java
+++ Winter 2022/examples/Lecture 2/java/LandscapeDemo/app/src/main/java/mobiledev/unb/ca/landscapedemo/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
-import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
@@ -12,7 +11,6 @@ import static android.content.res.Configuration.ORIENTATION_PORTRAIT;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button button;
     private Resources resources;
 
     @Override
@@ -20,23 +18,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        resources = button.getResources();
+        Button button = findViewById(R.id.button);
+        resources = getResources();
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View src) {
-                Context context = getApplicationContext();
+        button.setOnClickListener(src -> {
+            Context context = getApplicationContext();
 
-                if (resources.getConfiguration().orientation == ORIENTATION_PORTRAIT) {
-                    Toast.makeText(context,
-                              "Running in Portrait Mode",
-                                   Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(context,
-                              "Running in Landscape Mode",
-                                   Toast.LENGTH_SHORT).show();
-                }
+            if (resources.getConfiguration().orientation == ORIENTATION_PORTRAIT) {
+                Toast.makeText(context,
+                          "Running in Portrait Mode",
+                               Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(context,
+                          "Running in Landscape Mode",
+                               Toast.LENGTH_SHORT).show();
             }
         });
     }
