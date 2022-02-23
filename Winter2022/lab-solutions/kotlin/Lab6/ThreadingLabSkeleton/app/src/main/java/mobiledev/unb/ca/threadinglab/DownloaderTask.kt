464bf97f94c863c2448499ab857864640f7ca1ae
Howard Powell <hspowell@gmail.com>
2022-02-23 15:55:56 -0400

    Renamed folders

---
+++ Winter2022/lab-solutions/kotlin/Lab6/ThreadingLabSkeleton/app/src/main/java/mobiledev/unb/ca/threadinglab/DownloaderTask.kt
@@ -0,0 +1,96 @@
+package mobiledev.unb.ca.threadinglab
+
+import android.content.Context
+import android.os.Handler
+import android.widget.ProgressBar
+import androidx.recyclerview.widget.RecyclerView
+import android.os.Looper
+import android.widget.Button
+import mobiledev.unb.ca.threadinglab.model.GeoData
+import java.util.ArrayList
+import java.util.concurrent.Executors
+
+class DownloaderTask(private val activity: GeoDataListActivity) {
+    private val appContext: Context = activity.applicationContext
+    private var refreshButton: Button? = null
+    private var progressBar: ProgressBar? = null
+    private var recyclerView: RecyclerView? = null
+
+    fun setRefreshButton(refreshButton: Button?): DownloaderTask {
+        this.refreshButton = refreshButton
+        return this
+    }
+
+    fun setProgressBar(progressBar: ProgressBar?): DownloaderTask {
+        this.progressBar = progressBar
+        return this
+    }
+
+    fun setupRecyclerView(recyclerView: RecyclerView?): DownloaderTask {
+        this.recyclerView = recyclerView
+        return this
+    }
+
+    fun execute() {
+        // TODO
+        //  Disable the button so it can't be clicked again once a download has been started
+        //  Hint: Button is subclass of TextView. Read this document to see how to disable it.
+        //  http://developer.android.com/reference/android/widget/TextView.html
+
+        // TODO
+        //  Set the progress bar's maximum to be DOWNLOAD_TIME, its initial progress to be
+        //  0, and also make sure it's visible.
+        //  Hint: Read the documentation on ProgressBar
+        //  http://developer.android.com/reference/android/widget/ProgressBar.html
+
+        // Perform background call to read the information from the URL
+        Executors.newSingleThreadExecutor().execute {
+            val handler = Handler(Looper.getMainLooper())
+
+            // TODO
+            //  Create an instance of JsonUtils and get the data from it,
+            //  store the data in mGeoDataList
+
+            // Simulating long-running operation
+            for (i in 1 until DOWNLOAD_TIME) {
+                sleep()
+                // TODO
+                //  Update the progress bar using values
+            }
+        }
+    }
+
+    private fun sleep() {
+        try {
+            val mDelay = 500
+            Thread.sleep(mDelay.toLong())
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun updateDisplay(mGeoDataList: ArrayList<GeoData>) {
+        // TODO
+        //  With the download completed, enable the button again
+
+        // TODO
+        //  Reset the progress bar, and make it disappear
+
+        // TODO
+        //  Setup the RecyclerView
+
+        // TODO
+        //  Create a Toast indicating that the download is complete. Set its text
+        //  to be the result String from doInBackground
+    }
+
+    private fun setupRecyclerView(mGeoDataList: List<GeoData>) {
+        recyclerView!!.adapter =
+            SimpleItemRecyclerViewAdapter(mGeoDataList, activity, activity.isTwoPane)
+    }
+
+    companion object {
+        private const val DOWNLOAD_TIME = 4 // Download time simulation
+    }
+
+}
---
+++ Winter2022/lab-solutions/kotlin/Lab6/ThreadingLabSkeleton/app/src/main/java/mobiledev/unb/ca/threadinglab/model/GeoData.kt
@@ -0,0 +1,11 @@
+package mobiledev.unb.ca.threadinglab.model
+
+class GeoData  // Empty Constructor
+{
+    // Setter Methods
+    // Getter Methods
+    // GeoData model attributes
+    var title: String? = null
+    var longitude: String? = null
+    var latitude: String? = null
+}
