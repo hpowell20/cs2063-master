bcca5d242afc6a3aeb51bfdc81dd560328cfc64d
Howard Powell <hspowell@gmail.com>
2022-10-29 12:16:09 -0300

    Update to skeleton project

--- Fall2022/labs/Lab7/RoomPersistenceLabSkeleton/app/src/main/java/mobiledev/unb/ca/roompersistencelab/MainActivity.kt
+++ Fall2022/labs/Lab7/RoomPersistenceLabSkeleton/app/src/main/java/mobiledev/unb/ca/roompersistencelab/MainActivity.kt
@@ -2,7 +2,6 @@ package mobiledev.unb.ca.roompersistencelab
 
 import android.os.Bundle
 import android.view.KeyEvent
-import android.view.View
 import android.view.inputmethod.EditorInfo
 import android.widget.Button
 import android.widget.EditText
@@ -11,12 +10,11 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import mobiledev.unb.ca.roompersistencelab.ui.ItemViewModel
-import mobiledev.unb.ca.roompersistencelab.ui.ItemsAdapter
 
 class MainActivity : AppCompatActivity() {
-    private var mItemViewModel: ItemViewModel? = null
-    private val mItemsAdapter: ItemsAdapter? = null
-    private var mListView: ListView? = null
+    private lateinit var mItemViewModel: ItemViewModel
+    private lateinit var mListView: ListView
+
     private var mSearchEditText: EditText? = null
     private var mItemEditText: EditText? = null
     private var mNumberEditText: EditText? = null
@@ -34,14 +32,18 @@ class MainActivity : AppCompatActivity() {
 
         val mAddButton = findViewById<Button>(R.id.add_button)
         mAddButton.setOnClickListener {
-            // TODO
+            // TODO 1
             //  Check if some text has been entered in both the item and number EditTexts.
-            //  If so call the addItem method using the the text from these EditTexts.
             //  If not display a toast indicating that the data entered was incomplete.
+            //  HINT:
+            //    Have a look at the TextUtils class (https://developer.android.com/reference/android/text/TextUtils)
+
+            // TODO 2
+            //  Call the addItem method using the the text from these EditTexts.
         }
 
         mSearchEditText = findViewById(R.id.search_edit_text)
-        mSearchEditText?.setOnEditorActionListener { _: TextView?, actionId: Int, _: KeyEvent? ->
+        mSearchEditText?.setOnEditorActionListener { v: TextView?, actionId: Int, _: KeyEvent? ->
             if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                 // TODO
                 //  v is the search EditText. (EditText is a subclass of TextView.)
@@ -56,28 +58,33 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun addItem(item: String, num: String) {
-        // TODO
+        // TODO 1
         //  Make a call to the view model to create a record in the database table
 
-        // TODO
+        // TODO 2
         //  You will need to write a bit of extra code to get the
         //  UI to behave nicely, e.g., showing and hiding the keyboard
-        //  at the right time, clearing text fields appropriately. Some
-        //  of that code will likely go here, but you might also make
+        //  at the right time, clearing text fields appropriately.
+        //  Some of that code will likely go here, but you might also make
         //  changes elsewhere in the app. Exactly how you make the
         //  UI behave is up to you, but you should make reasonable
         //  choices.
+        //  HINT:
+        //    There is a utility object called KeyboardUtils which may be helpful here
     }
 
     private fun searchRecords(item: String) {
-        // TODO
+        // TODO 1
         //  Make a call to the view model to search for records in the database that match the query item.
         //  Make sure that the results are sorted appropriately
 
-        // TODO
+        // TODO 2
         //  Update the results section.
         //  If there are no results, set the results TextView to indicate that there are no results.
         //  If there are results, set the results TextView to indicate that there are results.
         //  Again, you might need to write a bit of extra code here or elsewhere, to get the UI to behave nicely.
+        //  HINT:
+        //    When displaying the results you will need to set the ItemsAdapter object and the
+        //    adapter attribute of mListView
     }
 }
