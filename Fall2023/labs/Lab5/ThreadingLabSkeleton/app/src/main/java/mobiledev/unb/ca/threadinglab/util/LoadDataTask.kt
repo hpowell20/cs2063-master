772a67069c19207f80e4ae1d7245777752f4568c
Howard Powell <hspowell@gmail.com>
2023-10-12 08:49:04 -0300

    Lab 5 skeleton

---
+++ Fall2023/labs/Lab5/ThreadingLabSkeleton/app/src/main/java/mobiledev/unb/ca/threadinglab/util/LoadDataTask.kt
@@ -0,0 +1,64 @@
+package mobiledev.unb.ca.threadinglab.util
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import android.os.Handler
+import android.os.Looper
+import mobiledev.unb.ca.threadinglab.MyAdapter
+import mobiledev.unb.ca.threadinglab.model.Course
+import java.util.concurrent.Executors
+
+class LoadDataTask(private val activity: AppCompatActivity) {
+    private val appContext: Context = activity.applicationContext
+    private var recyclerView: RecyclerView? = null
+
+    fun setRecyclerView(recyclerView: RecyclerView?): LoadDataTask {
+        this.recyclerView = recyclerView
+        return this
+    }
+
+    fun execute() {
+        Executors.newSingleThreadExecutor()
+            .execute {
+                val mainHandler = Handler(Looper.getMainLooper())
+                // TODO 1
+                //  Load the data from the JSON assets file and return the list of courses
+
+                // Simulate a long-running operation
+                for (i in 1 until DOWNLOAD_TIME) {
+                    sleep()
+                }
+
+                // TODO 2
+                //  Using the updateDisplay method update the UI with the results
+            }
+    }
+
+    private fun sleep() {
+        try {
+            val mDelay = 500
+            Thread.sleep(mDelay.toLong())
+        } catch (e: InterruptedException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun updateDisplay(courseList: ArrayList<Course>) {
+        // TODO 3
+        //  Setup the RecyclerView using the setupRecyclerView method
+
+        // TODO 4
+        //  Create a Toast indicating that the file has been loaded
+        //    HINT: Read this for help with Toast:
+        //    http://developer.android.com/guide/topics/ui/notifiers/toasts.html
+    }
+
+    private fun setupRecyclerView(courseList: ArrayList<Course>) {
+        recyclerView!!.adapter = MyAdapter(activity, courseList)
+    }
+
+    companion object {
+        private const val DOWNLOAD_TIME = 5 // Download time simulation
+    }
+}
