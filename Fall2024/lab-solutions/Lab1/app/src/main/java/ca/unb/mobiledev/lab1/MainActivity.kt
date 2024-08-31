d2358b62f03bf35d9a9c473d763db334d5bdb76b
Howard Powell <hspowell@gmail.com>
2024-08-31 12:30:42 -0300

    Lab 1 details

---
+++ Fall2024/lab-solutions/Lab1/app/src/main/java/ca/unb/mobiledev/lab1/MainActivity.kt
@@ -0,0 +1,28 @@
+package ca.unb.mobiledev.lab1
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.i(TAG, "This is a log display!")
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    companion object {
+        // String for LogCat documentation
+        private const val TAG = "Main Activity"
+    }
+}
