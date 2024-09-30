8143d6857f22f7433e5a33b80ad22cdc15be12b2
Howard Powell <hspowell@gmail.com>
2024-09-30 07:31:59 -0300

    Lab 5 skeleton files

---
+++ Fall2024/labs/Lab5/ThreadingLabSkeleton/app/src/main/java/mobiledev/unb/ca/threadinglab/MainActivity.kt
@@ -0,0 +1,18 @@
+package mobiledev.unb.ca.threadinglab
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // TODO 1
+        //  Get a reference to the RecyclerView
+
+        // TODO 2
+        //  Create an instance of LoadDataTask using this activity in the constructor
+        //  and use the setters to pass in the objects needed during execution
+    }
+}
