35d7c5c95b25fc0bf934056d58381dbf8acb63c0
Howard Powell <hspowell@gmail.com>
2024-10-12 21:47:27 -0300

    Read write demo updates

---
+++ Fall2024/examples/Lecture 11/ReadWriteDemo/app/src/main/java/ca/unb/mobiledev/readwritedemo/FileUtils.kt
@@ -0,0 +1,47 @@
+package ca.unb.mobiledev.readwritedemo
+
+import android.content.Context
+import android.util.Log
+import java.io.BufferedReader
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStreamReader
+
+class FileUtils(private val appContext: Context) {
+
+    fun writeDataToFile(fileName: String, fileData: String) {
+        val fileOutputStream: FileOutputStream
+        try {
+            fileOutputStream = appContext.openFileOutput(fileName, Context.MODE_PRIVATE)
+            fileOutputStream.write(fileData.toByteArray())
+        } catch (e: Exception) {
+            when (e) {
+                is FileNotFoundException -> Log.e(TAG, "File not found: " + e.message)
+                is IOException -> Log.e(TAG, "IO Exception: " + e.message)
+                else -> e.printStackTrace()
+            }
+        }
+    }
+
+    fun readDataFromFile(fileName: String): String {
+        val fileInputStream = appContext.openFileInput(fileName)
+        val inputStreamReader = InputStreamReader(fileInputStream)
+        val bufferedReader = BufferedReader(inputStreamReader)
+        val stringBuilder: StringBuilder = StringBuilder()
+
+        var text: String?
+        while (run {
+                text = bufferedReader.readLine()
+                text
+            } != null) {
+            stringBuilder.append(text)
+        }
+
+        return stringBuilder.toString()
+    }
+
+    companion object {
+        private const val TAG = "FileUtils"
+    }
+}
