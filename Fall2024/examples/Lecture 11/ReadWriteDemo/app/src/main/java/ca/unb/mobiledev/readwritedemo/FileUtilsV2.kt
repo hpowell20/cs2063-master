82c35f09e0847110518232495199914d16b4c31b
Howard Powell <hspowell@gmail.com>
2024-10-13 06:25:38 -0300

    Read Write demo updates

--- Fall2024/examples/Lecture 11/ReadWriteDemo/app/src/main/java/ca/unb/mobiledev/readwritedemo/FileUtilsV2.kt
+++ Fall2024/examples/Lecture 11/ReadWriteDemo/app/src/main/java/ca/unb/mobiledev/readwritedemo/FileUtilsV2.kt
@@ -2,18 +2,20 @@ package ca.unb.mobiledev.readwritedemo
 
 import android.content.Context
 import android.util.Log
+import ca.unb.mobiledev.readwritedemo.FileUtils.Companion
 import java.io.BufferedReader
 import java.io.FileNotFoundException
 import java.io.FileOutputStream
 import java.io.IOException
 import java.io.InputStreamReader
 
-class FileUtils(private val appContext: Context) {
+class FileUtilsV2(private val appContext: Context) {
 
     fun writeDataToFile(fileName: String, fileData: String) {
         try {
-            val fileOutputStream = appContext.openFileOutput(fileName, Context.MODE_PRIVATE)
-            fileOutputStream.write(fileData.toByteArray())
+            appContext.openFileOutput(fileName, Context.MODE_PRIVATE).use { fos ->
+                fos.write(fileData.toByteArray())
+            }
         } catch (e: Exception) {
             when (e) {
                 is FileNotFoundException -> Log.e(
@@ -30,23 +32,21 @@ class FileUtils(private val appContext: Context) {
     }
 
     fun readDataFromFile(fileName: String): String {
-        var fileContents = ""
+        return try {
+            appContext.openFileInput(fileName).use { fis ->
+                InputStreamReader(fis).use { isr ->
+                    BufferedReader(isr).use { br ->
+                        val stringBuilder = StringBuilder()
+                        br.useLines { lines ->
+                            lines.forEach { text ->
+                                stringBuilder.append(text)
+                            }
+                        }
 
-        try {
-            val fileInputStream = appContext.openFileInput(fileName)
-            val inputStreamReader = InputStreamReader(fileInputStream)
-            val bufferedReader = BufferedReader(inputStreamReader)
-            val stringBuilder: StringBuilder = StringBuilder()
-
-            var text: String?
-            while (run {
-                    text = bufferedReader.readLine()
-                    text
-                } != null) {
-                stringBuilder.append(text)
+                        stringBuilder.toString()
+                    }
+                }
             }
-
-            fileContents = stringBuilder.toString()
         } catch (e: IOException) {
             when (e) {
                 is FileNotFoundException -> Log.e(
@@ -55,12 +55,11 @@ class FileUtils(private val appContext: Context) {
                 )
                 else -> e.printStackTrace()
             }
+            ""
         }
-
-        return fileContents
     }
 
     companion object {
-        private const val TAG = "FileUtils"
+        private const val TAG = "FileUtilsV2"
     }
 }
